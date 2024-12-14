b75fb91f4a45067d436865530fc828013334c020
anton_chernianu <anton@chernianu.com>
2024-12-14 17:06:06 +0200

    fix: update file size

--- src/core/components/FileManager/FileManager.tsx
+++ src/core/components/FileManager/FileManager.tsx
@@ -50,6 +50,9 @@ export const FileManager = (props: FileManagerPropsType) => {
   };
 
   const formatSize = (bytes: number) => {
+    if (bytes < 1024 * 1024) {
+      return (bytes / 1024).toFixed(2) + " KB";
+    }
     return (bytes / (1024 * 1024)).toFixed(2) + " MB";
   };
 
